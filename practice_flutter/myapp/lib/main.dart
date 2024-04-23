dbb12e4198de7202a4b8b68216386e8f286582f2
SQCodes <paulsqmagadi@gmail.com>
2024-04-23 19:08:08 +0300

    modified:   practice_flutter/myapp/lib/main.dart

--- practice_flutter/myapp/lib/main.dart
+++ practice_flutter/myapp/lib/main.dart
@@ -1,44 +1,42 @@
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My First Flutter App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text(
-              'Hello, Flutter!',
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-                color: Colors.red,
-                backgroundColor: Color.fromARGB(255, 158, 152, 133),
-                decoration: TextDecoration.underline,
-              ),
-              textAlign: TextAlign.center,
-              ),
-            ),
-        ),
-        body: const Center(
-          child: Text(
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend commodo libero. Vestibulum in metus rhoncus, feugiat augue vel, varius nulla. Morbi tincidunt sem at nibh placerat vulputate. Maecenas bibendum metus sit amet nulla fermentum semper. Suspendisse suscipit tortor nisi, sed euismod mauris gravida eu. Nullam facilisis magna lacus, id imperdiet tellus scelerisque sodales. Fusce auctor nisl ex, a iaculis turpis hendrerit ac. Phasellus malesuada lorem ac ligula pharetra molestie. Integer sed sapien nunc. Aliquam ac magna tellus. Suspendisse consequat ipsum eget nulla maximus facilisis nec varius risus. Sed ut erat vel risus auctor tincidunt viverra vel orci. Vestibulum ac maximus leo. Quisque vitae lorem ultricies, tristique lectus ac, vulputate sem. Aliquam erat arcu, posuere in consequat id, ornare at elit. Sed tempus egestas magna, sit amet pulvinar enim molestie eu. Nulla consequat urna in facilisis cursus. Nullam rhoncus nibh felis, eu pellentesque turpis accumsan in. Ut porta lectus blandit orci hendrerit elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus ac magna lectus. Sed a quam euismod, fames ac ante ipsum primis in faucibus. Morbi pulvinar, augue quis egestas accumsan, eros eros viverra nibh, a facilisis leo urna finibus turpis. Sed vulputate neque nisl, eget posuere dolor consectetur porttitor.Pellentesque quis ligula eu erat pretium elementum. Donec a nisl ut nulla cursus efficitur et a nulla. Aliquam id leo at diam dapibus tempus. Praesent risus elit, eleifend a massa in, vulputate ultricies velit. Cras in diam et dolor tincidunt eleifend quis et enim. Phasellus.',
-            style: TextStyle(fontSize: 24),
-            overflow: TextOverflow.visible,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'My First Flutter App',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Center(
+//             child: Text(
+//               'Hello, Flutter!',
+//               style: TextStyle(
+//                 fontSize: 36,
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.red,
+//                 backgroundColor: Color.fromARGB(255, 158, 152, 133),
+//                 decoration: TextDecoration.underline,
+//               ),
+//               textAlign: TextAlign.center,
+//               ),
+//             ),
+//         ),
+//         body: const Center(
+//           child: Text(
+//             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend commodo libero. Vestibulum in metus rhoncus, feugiat augue vel, varius nulla. Morbi tincidunt sem at nibh placerat vulputate. Maecenas bibendum metus sit amet nulla fermentum semper. Suspendisse suscipit tortor nisi, sed euismod mauris gravida eu. Nullam facilisis magna lacus, id imperdiet tellus scelerisque sodales. Fusce auctor nisl ex, a iaculis turpis hendrerit ac. Phasellus malesuada lorem ac ligula pharetra molestie. Integer sed sapien nunc. Aliquam ac magna tellus. Suspendisse consequat ipsum eget nulla maximus facilisis nec varius risus. Sed ut erat vel risus auctor tincidunt viverra vel orci. Vestibulum ac maximus leo. Quisque vitae lorem ultricies, tristique lectus ac, vulputate sem. Aliquam erat arcu, posuere in consequat id, ornare at elit. Sed tempus egestas magna, sit amet pulvinar enim molestie eu. Nulla consequat urna in facilisis cursus. Nullam rhoncus nibh felis, eu pellentesque turpis accumsan in. Ut porta lectus blandit orci hendrerit elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus ac magna lectus. Sed a quam euismod, fames ac ante ipsum primis in faucibus. Morbi pulvinar, augue quis egestas accumsan, eros eros viverra nibh, a facilisis leo urna finibus turpis. Sed vulputate neque nisl, eget posuere dolor consectetur porttitor.Pellentesque quis ligula eu erat pretium elementum. Donec a nisl ut nulla cursus efficitur et a nulla. Aliquam id leo at diam dapibus tempus. Praesent risus elit, eleifend a massa in, vulputate ultricies velit. Cras in diam et dolor tincidunt eleifend quis et enim. Phasellus.',
+//             style: TextStyle(fontSize: 24),
+//             overflow: TextOverflow.visible,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
 
@@ -193,5 +191,57 @@ class MyApp extends StatelessWidget {
 //     ),
 //   );
 // }
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Center(
+//             child: Text("To Do App"),
+//           ),
+//           backgroundColor: Colors.deepPurple[400],
+//         ),
+//         body: const Center(
+//           child: Image(
+//             image: 
+//               AssetImage("images/paul.jpg")),
+//         ),
+//         backgroundColor: Colors.deepPurple[200],
+//       ),
+//     ),
+//   );
+// }
+
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text("To Do App"),
+          ),
+          backgroundColor: Colors.deepPurple[400],
+        ),
+        body: const Center(
+          child: Image(
+            image: 
+              NetworkImage('https://codemy.com/images/aspenhospital.jpg')),
+        ),
+        backgroundColor: Colors.deepPurple[200],
+      ),
+    ),
+  );
+}
+
+
+
 
 
