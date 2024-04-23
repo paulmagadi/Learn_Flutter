cc4bfb9b7dd291dc12e0669f1ebbd7b038962e85
SQCodes <paulsqmagadi@gmail.com>
2024-04-23 14:17:17 +0300

    deleted:    practice_flutter/.idea/.gitignore 	deleted:    practice_flutter/.idea/libraries/Dart_Packages.xml 	deleted:    practice_flutter/.idea/libraries/Dart_SDK.xml 	deleted:    practice_flutter/.idea/misc.xml 	deleted:    practice_flutter/.idea/modules.xml 	deleted:    practice_flutter/.idea/practice_flutter.iml 	deleted:    practice_flutter/.idea/vcs.xml 	deleted:    practice_flutter/Screenshot_20240422_170408.png 	modified:   practice_flutter/myapp/lib/main.dart 	modified:   practice_flutter/myapp/test/widget_test.dart

--- practice_flutter/myapp/lib/main.dart
+++ practice_flutter/myapp/lib/main.dart
@@ -1,3 +1,132 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       home: Text("To do app"),
+//     ),
+//   );
+// }
+
+//Center
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       home: Center(
+//         child: Text("To do app"),
+//       ),
+//     ),
+//   );
+// }
+
+// Scaffold
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text("To Do App"),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// Center
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Center(
+//            child:  Text("To Do App"),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Center(
+//             child: Text("To Do App"),
+//           ),
+//         ),
+//         body: Center(
+//           child: Text ("Learn to code"),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// Colors
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Center(
+//             child: Text("To Do App"),
+//           ),
+//           backgroundColor: Colors.deepPurple[400],
+//         ),
+//         body: const Center(
+//           child: Text ("Learn to code"),
+//         ),
+//         backgroundColor: Colors.deepPurple[200],
+//       ),
+//     ),
+//   );
+// }
+
+//Local Image
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Center(
+//             child: Text("To Do App"),
+//           ),
+//           backgroundColor: Colors.deepPurple[400],
+//         ),
+//         body: const Center(
+//           child: Image(
+//             image: 
+//               AssetImage("images/paul.jpg")),
+//         ),
+//         backgroundColor: Colors.deepPurple[200],
+//       ),
+//     ),
+//   );
+// }
+
+
+//Internet Image
+
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,11 +134,17 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My First App'),
+          title: const Center(
+            child: Text("To Do App"),
+          ),
+          backgroundColor: Colors.deepPurple[400],
         ),
         body: const Center(
-          child: Text('Hello World!'),
+          child: Image(
+            image: 
+              NetworkImage('https://codemy.com/images/aspenhospital.jpg')),
         ),
+        backgroundColor: Colors.deepPurple[200],
       ),
     ),
   );
