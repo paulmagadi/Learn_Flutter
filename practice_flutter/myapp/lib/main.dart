4818d3586699c0413ee9fe1fd3a5023944d8032d
SQCodes <paulsqmagadi@gmail.com>
2024-04-24 11:17:53 +0300

    modified:   practice_flutter/myapp/lib/main.dart

--- practice_flutter/myapp/lib/main.dart
+++ practice_flutter/myapp/lib/main.dart
@@ -280,6 +280,8 @@ class MyWidget extends StatelessWidget {
           title: const Text('Row In Flutter'),
         ),
         body: const Row(
+          // Try replacing "center" with "start", "end", "spaceBetween", "spaceAround" or "spaceEvenly"
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(Icons.star, color: Colors.black),
             Icon(Icons.star, color: Colors.black),
