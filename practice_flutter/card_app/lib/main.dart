8755282a633129ecc9a84479ff3770240dd142f1
SQCodes <paulsqmagadi@gmail.com>
2024-04-29 19:08:05 +0300

    modified:   practice_flutter/card_app/lib/main.dart

--- practice_flutter/card_app/lib/main.dart
+++ practice_flutter/card_app/lib/main.dart
@@ -5,20 +5,32 @@ import './screens/home.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Card',
       home: Home(),
-      routes: {},
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     SystemChrome.setSystemUIOverlayStyle(
+//         const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Card',
+//       home: Home(),
+//       routes: {},
+//     );
+//   }
+// }
