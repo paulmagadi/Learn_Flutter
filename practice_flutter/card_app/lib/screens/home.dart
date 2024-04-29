3e14e5feeff0e754627d9d9b4c819d270d587989
SQCodes <paulsqmagadi@gmail.com>
2024-04-29 13:23:46 +0300

    modified:   practice_flutter/card_app/lib/screens/home.dart

--- practice_flutter/card_app/lib/screens/home.dart
+++ practice_flutter/card_app/lib/screens/home.dart
@@ -18,118 +18,126 @@ class _HomeState extends State<Home> {
           foregroundColor: whiteColor,
           title: const Text("Flutter Card"),
         ),
-        body: Card(
-          elevation: 4.0,
-          margin: const EdgeInsets.all(26),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const Row(
-                      children: [
-                        Icon(Icons.phone, color: mainColor),
-                        SizedBox(width: 8), // Add spacing between icon and text
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+        body: Column(
+          children: [
+            Card(
+              elevation: 4.0,
+              margin: const EdgeInsets.all(26),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const Row(
                           children: [
-                            Text('Tell',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text('+2547000000',
-                                style: TextStyle(color: greyColor)),
+                            Icon(Icons.phone, color: mainColor),
+                            SizedBox(width: 8), // Add spacing between icon and text
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Tell',
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                Text('+2547000000',
+                                    style: TextStyle(color: greyColor)),
+                              ],
+                            ),
                           ],
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 16), // Add spacing between rows
-
-                    // Email information
-                    const Row(
-                      children: [
-                        Icon(Icons.email, color: mainColor),
-                        SizedBox(width: 8),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        const SizedBox(height: 16), // Add spacing between rows
+            
+                        // Email information
+                        const Row(
                           children: [
-                            Text('Email',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text('sqcodes@gmail.com',
-                                style: TextStyle(color: greyColor)),
+                            Icon(Icons.email, color: mainColor),
+                            SizedBox(width: 8),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Email',
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                Text('sqcodes@gmail.com',
+                                    style: TextStyle(color: greyColor)),
+                              ],
+                            ),
                           ],
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 16), // Add spacing between rows
-
-                    // Website information
-                    Row(
-                      children: [
-                        const Icon(Icons.web, color: mainColor),
-                        const SizedBox(width: 8),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        const SizedBox(height: 16), // Add spacing between rows
+            
+                        // Website information
+                        Row(
                           children: [
-                            const Text('Website',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            GestureDetector(
-                              onTap: () {
-                                // Handle website tap
-                                // You can use url_launcher package to launch the URL
-                              },
-                              child: const Text(
-                                'sqcodes.com',
-                                style: TextStyle(color: greyColor),
-                              ),
+                            const Icon(Icons.web, color: mainColor),
+                            const SizedBox(width: 8),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text('Website',
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                GestureDetector(
+                                  onTap: () {
+                                    // Handle website tap
+                                    // You can use url_launcher package to launch the URL
+                                  },
+                                  child: const Text(
+                                    'sqcodes.com',
+                                    style: TextStyle(color: greyColor),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 16), // Add spacing between rows
+            
+                        // Address information
+                        const Row(
+                          children: [
+                            Icon(Icons.location_on, color: mainColor),
+                            SizedBox(width: 8),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Address',
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                Text('Nairobi, Kenya',
+                                    style: TextStyle(color: greyColor)),
+                              ],
                             ),
                           ],
                         ),
                       ],
                     ),
-                    const SizedBox(height: 16), // Add spacing between rows
-
-                    // Address information
-                    const Row(
-                      children: [
-                        Icon(Icons.location_on, color: mainColor),
-                        SizedBox(width: 8),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Address',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text('Nairobi, Kenya',
-                                style: TextStyle(color: greyColor)),
-                          ],
+                    Column(
+                      children: <Widget>[
+                        const CircleAvatar(
+                          radius: 50,
+                          backgroundImage: AssetImage('images/paul.jpg'),
                         ),
+                        const SizedBox(height: 20),
+                        Text('Paul Magadi',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyLarge
+                                ?.copyWith(fontWeight: FontWeight.bold)),
+                        const SizedBox(height: 20),
+                        Text('Software Developer ',
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.bodyLarge),
+                        const SizedBox(height: 20),
                       ],
-                    ),
+                    )
                   ],
                 ),
-                Column(
-                  children: <Widget>[
-                    const CircleAvatar(
-                      radius: 50,
-                      backgroundImage: AssetImage('images/paul.jpg'),
-                    ),
-                    const SizedBox(height: 20),
-                    Text('Paul Magadi',
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyLarge
-                            ?.copyWith(fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 20),
-                    Text('Software Developer ',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.bodyLarge),
-                    const SizedBox(height: 20),
-                  ],
-                )
-              ],
+              ),
             ),
-          ),
+            Column(
+              
+            )
+          ],
+          
         ),
       ),
     );
