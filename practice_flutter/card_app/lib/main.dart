1085a9312db7e9c188d6a43168cff01f58998580
SQCodes <paulsqmagadi@gmail.com>
2024-04-29 19:08:50 +0300

    modified:   practice_flutter/card_app/lib/main.dart

--- practice_flutter/card_app/lib/main.dart
+++ practice_flutter/card_app/lib/main.dart
@@ -5,12 +5,12 @@ import './screens/home.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
