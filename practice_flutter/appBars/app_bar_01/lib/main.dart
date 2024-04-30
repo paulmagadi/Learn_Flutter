8fd145401de1776b243837c7a6725a3b273b98ad
SQCodes <paulsqmagadi@gmail.com>
2024-04-30 17:24:57 +0300

    modified:   practice_flutter/appBars/app_bar_01/lib/main.dart 	new file:   practice_flutter/appBars/app_bar_01/lib/screens/home_page.dart

--- practice_flutter/appBars/app_bar_01/lib/main.dart
+++ practice_flutter/appBars/app_bar_01/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter App Bar',
-      theme: ThemeData(
-        
-      ),
-      home: const MyHomePage(),
+      theme: ThemeData(),
+      home: const HomePage(),
     );
   }
 }
-
-
-
-
---
+++ practice_flutter/appBars/app_bar_01/lib/screens/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(),
+      body:const Column(
+        children: [],
+      ),
+    );
+  }
+}
+
+_appBar() {
+  return AppBar(
+    leading: GestureDetector(
+      onTap: () {},
+      child: const Icon(
+        Icons.dark_mode_rounded,
+        size: 20,
+      ),
+    ),
+  );
+}
