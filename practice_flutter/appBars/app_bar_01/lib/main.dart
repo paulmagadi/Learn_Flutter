fab35f37b47a542c82fc01a54ff17838ff69d3b2
SQCodes <paulsqmagadi@gmail.com>
2024-04-30 17:11:09 +0300

    modified:   practice_flutter/appBars/app_bar_01/lib/main.dart

--- practice_flutter/appBars/app_bar_01/lib/main.dart
+++ practice_flutter/appBars/app_bar_01/lib/main.dart
@@ -28,7 +28,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Color.fromARGB(255, 0, 86, 82)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
