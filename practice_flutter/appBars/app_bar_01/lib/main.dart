82f72c5f8321b1ce9592fd7e2ea463a029c001f5
SQCodes <paulsqmagadi@gmail.com>
2024-04-30 20:10:00 +0300

    modified:   practice_flutter/appBars/app_bar_01/lib/main.dart

--- practice_flutter/appBars/app_bar_01/lib/main.dart
+++ practice_flutter/appBars/app_bar_01/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './screens/home_page.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter App Bar',
