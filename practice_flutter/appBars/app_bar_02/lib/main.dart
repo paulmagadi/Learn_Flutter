9696843ce01555610101275f3c66ed1ce6ce07af
SQCodes <paulsqmagadi@gmail.com>
2024-04-30 20:43:47 +0300

    modified:   practice_flutter/appBars/app_bar_02/lib/main.dart

--- practice_flutter/appBars/app_bar_02/lib/main.dart
+++ practice_flutter/appBars/app_bar_02/lib/main.dart
@@ -10,16 +10,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("AppMaking.co"),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        leading: Icon(Icons.menu),
-        actions: [
-          IconButton(onPressed: () => {}, icon: Icon(Icons.search)),
-          IconButton(onPressed: () => {}, icon: Icon(Icons.more_vert)),
-        ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 8, 64, 10),
+          title: const Text("App Bar"),
+          centerTitle: true,
+          leading: const Icon(Icons.menu),
+          actions: [
+            IconButton(onPressed: () => {}, icon: Icon(Icons.search)),
+            IconButton(onPressed: () => {}, icon: Icon(Icons.more_vert)),
+          ],
+        ),
       ),
     );
   }
