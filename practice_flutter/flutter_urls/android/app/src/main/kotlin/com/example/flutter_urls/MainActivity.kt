fe2bf0961bfa6b91d20d7aeca7d0c4c6611defcd
SQCodes <paulsqmagadi@gmail.com>
2024-05-01 00:05:20 +0300

    new file:   practice_flutter/flutter_urls/lib/main.dart

---
+++ practice_flutter/flutter_urls/android/app/src/main/kotlin/com/example/flutter_urls/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_urls
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ practice_flutter/flutter_urls/lib/main.dart
@@ -0,0 +1,89 @@
+// importing dependencies
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+// function to trigger the build process
+void main() => runApp(const MyApp());
+
+_launchURLBrowser() async {
+var url = Uri.parse("https://www.geeksforgeeks.org/");
+if (await canLaunchUrl(url)) {
+	await launchUrl(url);
+} else {
+	throw 'Could not launch $url';
+}
+}
+
+_launchURLApp() async {
+var url = Uri.parse("https://www.geeksforgeeks.org/");
+if (await canLaunchUrl(url)) {
+	await launchUrl(url);
+} else {
+	throw 'Could not launch $url';
+}
+}
+
+class MyApp extends StatelessWidget {
+const MyApp({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+	return MaterialApp(
+	home: Scaffold(
+		appBar: AppBar(
+		title: const Text('Geeks for Geeks'),
+		backgroundColor: Colors.green,
+		),
+		body: SafeArea(
+		child: Center(
+			child: Column(
+			children: [
+				Container(
+				height: 250.0,
+				),
+				const Text(
+				'Welcome to GFG!',
+				style: TextStyle(
+					fontSize: 30.0,
+					color: Colors.green,
+					fontWeight: FontWeight.bold,
+				),
+				),
+				Container(
+				height: 20.0,
+				),
+				ElevatedButton(
+				onPressed: _launchURLBrowser,
+				style: ButtonStyle(
+					padding:
+						MaterialStateProperty.all(const EdgeInsets.all(5.0)),
+					textStyle: MaterialStateProperty.all(
+					const TextStyle(color: Colors.black),
+					),
+				),
+				// textColor: Colors.black,
+				// padding: const EdgeInsets.all(5.0),
+				child: const Text('Open in Browser'),
+				),
+				Container(
+				height: 20.0,
+				),
+				ElevatedButton(
+				onPressed: _launchURLApp,
+				style: ButtonStyle(
+					padding:
+						MaterialStateProperty.all(const EdgeInsets.all(5)),
+					textStyle: MaterialStateProperty.all(
+						const TextStyle(color: Colors.black))),
+				// textColor: Colors.black,
+				// padding: const EdgeInsets.all(5.0),
+				child: const Text('Open in App'),
+				),
+			],
+			),
+		),
+		),
+	),
+	);
+}
+}
