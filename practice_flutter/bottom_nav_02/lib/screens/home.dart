a7b2c12b3af70a2cb968a2bdc55dcce673555f20
SQCodes <paulsqmagadi@gmail.com>
2024-05-02 12:14:01 +0300

    modified:   practice_flutter/bottom_nav_02/lib/screens/home.dart

--- practice_flutter/bottom_nav_02/lib/screens/home.dart
+++ practice_flutter/bottom_nav_02/lib/screens/home.dart
@@ -18,17 +18,20 @@ class _HomeState extends State<Home> {
       body: const Column(
         children: [],
       ),
-      bottomNavigationBar:
-  
-          BottomNavigationBar(
-            backgroundColor: Colors.white,
-            elevation: 0,
-        items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Menu"),
-        BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
-        BottomNavigationBarItem(icon: Icon(Icons.call), label: "Call"),
-        
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          iconSize: 20,
+          mouseCursor: SystemMouseCursors.grab,
+          selectedFontSize: 20,
+          selectedIconTheme: const IconThemeData(color: Colors.amberAccent, size: 40),
+          selectedItemColor: Colors.amberAccent,
+          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Menu"),
+            BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
+            BottomNavigationBarItem(icon: Icon(Icons.call), label: "Call"),
+          ]),
     ));
   }
 }
