1a8a5b971745007befd52f8908268f2dc1287053
SQCodes <paulsqmagadi@gmail.com>
2024-05-03 16:47:44 +0300

    modified:   practice_flutter/bottom_nav_04/lib/constants/constants.dart

--- practice_flutter/bottom_nav_04/lib/constants/constants.dart
+++ practice_flutter/bottom_nav_04/lib/constants/constants.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 const Color mainColor = Color.fromARGB(255, 255, 98, 1);
 const Color secondaryColor = Colors.grey;
-const Color whiteColor = Color.fromARGB(255, 235, 229, 229);
+const Color whiteColor = Colors.white;
 const Color bgColor = Color.fromARGB(255, 9, 102, 102);
 const Color tintColor = Color.fromARGB(255, 0, 255, 204);
