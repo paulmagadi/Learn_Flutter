257a8111494298b6a2b0ef2fb9fdef1466295bdb
SQCodes <paulsqmagadi@gmail.com>
2024-05-03 16:14:04 +0300

    new file:   practice_flutter/bottom_nav_04/lib/constants/constants.dart 	modified:   practice_flutter/bottom_nav_04/lib/main.dart 	modified:   practice_flutter/bottom_nav_04/pubspec.yaml

---
+++ practice_flutter/bottom_nav_04/lib/constants/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color mainColor = Color.fromARGB(255, 255, 98, 1);
+const Color secondaryColor = Colors.grey;
+const Color whiteColor = Colors.white;
+const Color bgColor = Color.fromARGB(255, 9, 102, 102);
--- practice_flutter/bottom_nav_04/lib/main.dart
+++ practice_flutter/bottom_nav_04/lib/main.dart
@@ -1,139 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import './constants/constants.dart';
 
 // Define separate widgets for each screen
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Card(
-            margin: const EdgeInsets.all(20),
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //left column
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      //first child of left column
-                      Row(
-                        children: [
-                          IconButton(
-                              onPressed: () {}, icon: const Icon(Icons.call)),
-                          const SizedBox(width: 8),
-                          const Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Tell",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                              Text("+254700000001",
-                                  style: TextStyle(color: Colors.grey)),
-                            ],
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 25),
-                      //second child of left column
-                      Row(
-                        children: [
-                          IconButton(
-                              onPressed: () {}, icon: const Icon(Icons.email)),
-                          const SizedBox(width: 8),
-                          const Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Email",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                              Text("sqcodes@gmail.com",
-                                  style: TextStyle(color: Colors.grey)),
-                            ],
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 25),
-                      //Third child of left column
-                      Row(
-                        children: [
-                          IconButton(
-                              onPressed: () {}, icon: const Icon(Icons.web)),
-                          const SizedBox(width: 8),
-                          const Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Website",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                              Text("sqcodes.com",
-                                  style: TextStyle(color: Colors.grey)),
-                            ],
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 25),
-                      //fourth child of left column
-                      Row(
-                        children: [
-                          IconButton(
-                              onPressed: () {},
-                              color: Colors.orange,
-                              icon: const Icon(Icons.location_on)),
-                          const SizedBox(width: 8),
-                          const Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Address",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                              Text("Nairobi, Kenya",
-                                  style: TextStyle(color: Colors.grey)),
-                            ],
-                          )
-                        ],
-                      ),
-                      // const SizedBox(height: 25),
-                    ],
-                  ),
-                  //right column
-                  Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: Colors.orange,
-                            width: 4,
-                          ),
-                        ),
-                        child: const CircleAvatar(
-                          backgroundImage: AssetImage("images/paul.jpg"),
-                          radius: 70,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      const Text(
-                        "Paul Magadi",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 30),
-                      ),
-                      const Text(
-                        "Software Developer",
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
+    return const Center(
+      child: Text(
+        'Profile Screen',
       ),
     );
   }
@@ -156,9 +32,156 @@ class CameraScreen extends StatelessWidget {
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Profile Screen',
+    return SafeArea(
+      child: Column(
+        children: [
+          Card(
+            shadowColor: mainColor,
+            // borderOnForeground: false,
+
+            margin: const EdgeInsets.all(20),
+            elevation: 4.0,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //left column
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //first child of left column
+                      Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              color: mainColor,
+                              icon: const Icon(Icons.call)),
+                          const SizedBox(width: 8),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Tell",
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                              Text("+254700000001",
+                                  style: TextStyle(
+                                    color: mainColor,
+                                  )),
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 25),
+                      //second child of left column
+                      Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              color: mainColor,
+                              icon: const Icon(Icons.email)),
+                          const SizedBox(width: 8),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Email",
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                              Text("sqcodes@gmail.com",
+                                  style: TextStyle(
+                                    color: mainColor,
+                                  )),
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 25),
+                      //Third child of left column
+                      Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              color: mainColor,
+                              icon: const Icon(Icons.web)),
+                          const SizedBox(width: 8),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Website",
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                              Text("sqcodes.com",
+                                  style: TextStyle(
+                                    color: mainColor,
+                                  )),
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 25),
+                      //fourth child of left column
+                      Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              color: mainColor,
+                              icon: const Icon(Icons.location_on)),
+                          const SizedBox(width: 8),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Address",
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                              Text("Nairobi, Kenya",
+                                  style: TextStyle(
+                                    color: mainColor,
+                                  )),
+                            ],
+                          )
+                        ],
+                      ),
+                      // const SizedBox(height: 25),
+                    ],
+                  ),
+                  //right column
+                  Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: mainColor,
+                            width: 4,
+                          ),
+                        ),
+                        child: const CircleAvatar(
+                          backgroundImage: AssetImage("images/paul.jpg"),
+                          radius: 70,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text(
+                        "Paul Magadi",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30),
+                      ),
+                      const Text(
+                        "Software Developer",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: mainColor,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
@@ -198,15 +221,37 @@ class _MyAppState extends State<MyApp> {
         // Set the body according to the selected index
         body: _screens[_selectedIndex],
         bottomNavigationBar: CurvedNavigationBar(
-          backgroundColor: Colors.white,
-          color: Colors.blue,
-          buttonBackgroundColor: const Color.fromARGB(255, 66, 204, 231),
+          backgroundColor: whiteColor,
+          color: bgColor,
+          buttonBackgroundColor: bgColor,
           height: 60,
+          // animationCurve: Curves.easeOut,
+          // animationDuration: const Duration(microseconds: 10),
           items: const <Widget>[
-            Icon(Icons.menu, size: 30),
-            Icon(Icons.message, size: 30),
-            Icon(Icons.camera, size: 30),
-            Icon(Icons.person, size: 30),
+            Icon(
+              Icons.menu,
+              size: 30,
+              color: mainColor,
+              semanticLabel: "Menu",
+            ),
+            Icon(
+              Icons.message,
+              size: 30,
+              color: mainColor,
+              semanticLabel: "Message",
+            ),
+            Icon(
+              Icons.camera,
+              size: 30,
+              color: mainColor,
+              semanticLabel: "Camera",
+            ),
+            Icon(
+              Icons.person,
+              size: 30,
+              color: mainColor,
+              semanticLabel: "Profile",
+            ),
           ],
           onTap: (index) {
             // Update the state when an item is tapped
