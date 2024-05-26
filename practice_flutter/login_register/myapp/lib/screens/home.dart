64b0436be5bd632fd03c6050b6114663167bd9c3
SQCodes <paulsqmagadi@gmail.com>
2024-05-26 20:41:39 +0300

    modified:   practice_flutter/login_register/myapp/lib/screens/home.dart

--- practice_flutter/login_register/myapp/lib/screens/home.dart
+++ practice_flutter/login_register/myapp/lib/screens/home.dart
@@ -5,12 +5,17 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Hello"
-        ),
-      ),
-    );
+    return  Scaffold(
+      body: Container(
+  height: 100.0, // Height of the container
+  width: 100.0, // Width of the container
+  color: Colors.blue, // Background color
+  alignment: Alignment.center, // Align the child to the center
+  child: Text('Hello World'), // Child widget
+  padding: EdgeInsets.all(10.0), // Padding inside the container
+  margin: EdgeInsets.all(20.0), // Margin outside the container
+)
+        );
+      
   }
 }
