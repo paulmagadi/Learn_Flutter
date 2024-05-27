9ca8c49bdc1ae900d418bf869dfaf64546900542
SQCodes <paulsqmagadi@gmail.com>
2024-05-27 15:06:16 +0300

    modified:   practice_flutter/login_register/myapp/lib/home_page.dart

--- practice_flutter/login_register/myapp/lib/home_page.dart
+++ practice_flutter/login_register/myapp/lib/home_page.dart
@@ -37,6 +37,70 @@ class _HomePageState extends State<HomePage> {
     Widget build(BuildContext context) {
         return Scaffold(
             appBar: SearchAppBar(title: 'GreenFields'),
+            drawer: Drawer(
+        elevation: 0,
+        child: Column(
+          children: [
+            const UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/paul.jpg'),
+              ),
+              accountName: Text('Paul SQ Magadi'),
+              accountEmail: Text('paul.learner@plp.com'),
+            ),
+            // List Tile inside the Drawer
+            
+            ListTile(
+              title: const Text('About'),
+              leading: const Icon(
+                Icons.info,
+                color: Colors.blue,
+              ),
+              onTap: () {
+                // Navigate to the About page
+                Navigator.pushNamed(context, '/about');
+              },
+            ),
+            ListTile(
+              title: const Text('Help'),
+              leading: const Icon(
+                Icons.help_center,
+                color: Colors.blue,
+              ),
+              onTap: () {
+                // Navigate to the Help page
+                Navigator.pushNamed(context, '/help');
+              },
+            ),
+            ListTile(
+              title: const Text('Calendar'),
+              leading: const Icon(
+                Icons.calendar_today,
+                color: Colors.blue,
+              ),
+              onTap: () {
+                // Navigate to the Calendar page
+                Navigator.pushNamed(context, '/calendar');
+              },
+            ),
+            ListTile(
+              title: const Text('Logout'),
+              leading: const Icon(
+                Icons.logout,
+                color: Colors.blue,
+              ),
+              onTap: () {
+                // Handle logout action
+                // Add your logout logic here, e.g., sign out the user, navigate to the login screen
+                // Navigator.pushReplacementNamed(context, '/login');
+              },
+            ),
+          ],
+        ),
+      ),
             body: _pages[_selectedIndex],
             bottomNavigationBar: CurvedNavigationBar(
                 items: [
