6c935295dfb8df861dd586df1e0f00ba487e2ab0
SQCodes <paulsqmagadi@gmail.com>
2024-05-27 17:40:26 +0300

    modified:   practice_flutter/login_register/myapp/lib/home_page.dart

--- practice_flutter/login_register/myapp/lib/home_page.dart
+++ practice_flutter/login_register/myapp/lib/home_page.dart
@@ -1,43 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'appbar.dart';
 
 import 'screens/cart.dart';
 import 'screens/category.dart';
 import 'screens/deals.dart';
-import 'screens/home.dart';  
+import 'screens/home.dart';
 import 'screens/profile.dart';
 
 import 'package:curved_navigation_bar_with_label/curved_navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
-    const HomePage({super.key});
+  const HomePage({super.key});
 
-    @override
-    State<HomePage> createState() => _HomePageState();
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-    int _selectedIndex = 0;
+  int _selectedIndex = 0;
 
-    final List<Widget> _pages = [
-        const HomeScreen(),
-        const CategoryScreen(),
-        const ProfileScreen(),
-        const CartScreen(),
-        const DealsScreen(),
-    ];
+  final List<Widget> _pages = [
+    const CategoryScreen(),
+    const DealsScreen(),
+    const HomeScreen(),
+    const CartScreen(),
+    const ProfileScreen(),
+  ];
 
-    void _onItemTapped(int index) {
-        setState(() {
-            _selectedIndex = index;
-        });
-    }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: SearchAppBar(title: 'GreenFields'),
-            drawer: Drawer(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: SearchAppBar(title: 'GreenFields'),
+      drawer: Drawer(
         elevation: 0,
         child: Column(
           children: [
@@ -46,8 +47,8 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.blue,
               ),
               currentAccountPicture: CircleAvatar(
-                // backgroundImage: AssetImage('assets/images/paul.jpg'),
-              ),
+                  // backgroundImage: AssetImage('assets/images/paul.jpg'),
+                  ),
               accountName: Text('Paul SQ Magadi'),
               accountEmail: Text('paul.learner@plp.com'),
             ),
@@ -63,7 +64,7 @@ class _HomePageState extends State<HomePage> {
                 Navigator.pushNamed(context, '/about');
               },
             ),
-            
+
             ListTile(
               title: const Text('About'),
               leading: const Icon(
@@ -112,18 +113,23 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-            body: _pages[_selectedIndex],
-            bottomNavigationBar: CurvedNavigationBar(
-                items: [
-                    CurvedNavigationBarItem(icon: const Icon(Icons.home), label: "Home"),
-                    CurvedNavigationBarItem(icon: const Icon(Icons.category), label: "Category"),
-                    CurvedNavigationBarItem(icon: const Icon(Icons.local_offer), label: "Deals"),
-                    CurvedNavigationBarItem(icon: const Icon(Icons.shopping_cart), label: "Cart"),
-                    CurvedNavigationBarItem(icon: const Icon(Icons.person), label: "Account"),
-                ],
-                index: _selectedIndex,
-                onTap: _onItemTapped,
-            ),
-        );
-    }
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 50.00,
+          items: [
+          CurvedNavigationBarItem(
+              icon: const Icon(Icons.category, size: 20,), label: "Category"),
+          CurvedNavigationBarItem(
+              icon: const Icon(Icons.local_offer, size: 20,), label: "Deals"),
+          CurvedNavigationBarItem(icon: const Icon(Icons.home, size: 20,), label: "Home"),
+          CurvedNavigationBarItem(
+              icon: const Icon(Icons.shopping_cart, size: 20,), label: "Cart"),
+          CurvedNavigationBarItem(
+              icon: const Icon(Icons.person, size: 20,), label: "Account"),
+        ],
+        index: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
 }
