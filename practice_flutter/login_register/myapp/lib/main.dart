50891e467bd1e5e43ea63977d733b9af8a6974cb
SQCodes <paulsqmagadi@gmail.com>
2024-05-27 15:13:47 +0300

    modified:   practice_flutter/login_register/myapp/lib/main.dart

--- practice_flutter/login_register/myapp/lib/main.dart
+++ practice_flutter/login_register/myapp/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
+import 'screens/static/about.dart';
+import 'screens/static/calendar.dart';
+import 'screens/static/help.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,8 +20,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/about': (context) => AboutPage(), //Route to About Page/screen
         '/help': (context) => HelpPage(), //Route to Help Page/screen
-        '/calendar': (context) =>
-            CalendarPage(), //Route to Calendar Page/screen
+        '/calendar': (context) => CalendarPage(), //Route to Calendar Page/screen
       }, //Home view
     );
   }
