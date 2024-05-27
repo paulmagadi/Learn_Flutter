1239dc28f5787e031f5a48562f7609a8129233e9
SQCodes <paulsqmagadi@gmail.com>
2024-05-27 20:54:39 +0300

    modified:   practice_flutter/login_register/myapp/lib/home_page.dart

--- practice_flutter/login_register/myapp/lib/home_page.dart
+++ practice_flutter/login_register/myapp/lib/home_page.dart
@@ -38,71 +38,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SearchAppBar(title: 'GreenFields'),
-      drawer: Drawer(
-        elevation: 0,
-        child: Column(
-          children: [
-            const UserAccountsDrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              currentAccountPicture: CircleAvatar(
-                  // backgroundImage: AssetImage('assets/images/paul.jpg'),
-                  ),
-              accountName: Text('Paul SQ Magadi'),
-              accountEmail: Text('paul.learner@plp.com'),
-            ),
-            // List Tile inside the Drawer
-            ListTile(
-              title: const Text('Contact Us'),
-              leading: const Icon(
-                Icons.message,
-                color: Colors.blue,
-              ),
-              onTap: () {
-                // Navigate to the About page
-                Navigator.pushNamed(context, '/contact');
-              },
-            ),
-
-            ListTile(
-              title: const Text('About'),
-              leading: const Icon(
-                Icons.info,
-                color: Colors.blue,
-              ),
-              onTap: () {
-                // Navigate to the About page
-                Navigator.pushNamed(context, '/about');
-              },
-            ),
-            ListTile(
-              title: const Text('Help'),
-              leading: const Icon(
-                Icons.help_center,
-                color: Colors.blue,
-              ),
-              onTap: () {
-                // Navigate to the Help page
-                Navigator.pushNamed(context, '/help');
-              },
-            ),
-            
-            ListTile(
-              title: const Text('Logout'),
-              leading: const Icon(
-                Icons.logout,
-                color: Colors.blue,
-              ),
-              onTap: () {
-                // Handle logout action
-                // Add your logout logic here, e.g., sign out the user, navigate to the login screen
-                // Navigator.pushReplacementNamed(context, '/login');
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: drawer(context),
       body: _pages[_selectedIndex],
       bottomNavigationBar: CurvedNavigationBar(
         height: 50.00,
@@ -119,6 +55,74 @@ class _HomePageState extends State<HomePage> {
         ],
         index: _selectedIndex,
         onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Drawer drawer(BuildContext context) {
+    return Drawer(
+      elevation: 0,
+      child: Column(
+        children: [
+          const UserAccountsDrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            currentAccountPicture: CircleAvatar(
+                // backgroundImage: AssetImage('assets/images/paul.jpg'),
+                ),
+            accountName: Text('Paul SQ Magadi'),
+            accountEmail: Text('paul.learner@plp.com'),
+          ),
+          // List Tile inside the Drawer
+          ListTile(
+            title: const Text('Contact Us'),
+            leading: const Icon(
+              Icons.message,
+              color: Colors.blue,
+            ),
+            onTap: () {
+              // Navigate to the About page
+              Navigator.pushNamed(context, '/contact');
+            },
+          ),
+
+          ListTile(
+            title: const Text('About'),
+            leading: const Icon(
+              Icons.info,
+              color: Colors.blue,
+            ),
+            onTap: () {
+              // Navigate to the About page
+              Navigator.pushNamed(context, '/about');
+            },
+          ),
+          ListTile(
+            title: const Text('Help'),
+            leading: const Icon(
+              Icons.help_center,
+              color: Colors.blue,
+            ),
+            onTap: () {
+              // Navigate to the Help page
+              Navigator.pushNamed(context, '/help');
+            },
+          ),
+          
+          ListTile(
+            title: const Text('Logout'),
+            leading: const Icon(
+              Icons.logout,
+              color: Colors.blue,
+            ),
+            onTap: () {
+              // Handle logout action
+              // Add your logout logic here, e.g., sign out the user, navigate to the login screen
+              // Navigator.pushReplacementNamed(context, '/login');
+            },
+          ),
+        ],
       ),
     );
   }
