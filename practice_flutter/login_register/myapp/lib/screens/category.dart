02469f4f9b20d379aff68182f4342ca6199ad2dc
SQCodes <paulsqmagadi@gmail.com>
2024-05-28 22:08:00 +0300

    modified:   practice_flutter/login_register/myapp/lib/screens/category.dart

--- practice_flutter/login_register/myapp/lib/screens/category.dart
+++ practice_flutter/login_register/myapp/lib/screens/category.dart
@@ -15,29 +15,29 @@ class CategoriesScreen extends StatelessWidget {
 }
 
 class CategoryView extends StatelessWidget {
-  const CategoryView({
-    super.key,
-  });
+  const CategoryView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: GridView(
-        padding: const EdgeInsets.all(15),
-        children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(
-                  id: catData.id,
-                  title: catData.title,
-                  imageUrl: catData.imageUrl,
-                ))
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView.builder(
+      padding: const EdgeInsets.all(15),
+      shrinkWrap: true, // Add this line
+      physics: NeverScrollableScrollPhysics(), // Add this line
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
+      itemCount: DUMMY_CATEGORIES.length,
+      itemBuilder: (ctx, index) {
+        final catData = DUMMY_CATEGORIES[index];
+        return CategoryItem(
+          id: catData.id,
+          title: catData.title,
+          imageUrl: catData.imageUrl,
+        );
+      },
     );
   }
 }
