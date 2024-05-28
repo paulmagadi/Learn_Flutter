59181e2647a6c9bd8918a334640b93307f36e4e6
SQCodes <paulsqmagadi@gmail.com>
2024-05-28 20:42:30 +0300

    modified:   practice_flutter/login_register/myapp/lib/screens/category.dart

--- practice_flutter/login_register/myapp/lib/screens/category.dart
+++ practice_flutter/login_register/myapp/lib/screens/category.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import '../models/category.dart';
+import '../widgets/category_item.dart';
+import '../main.dart'; // Import DUMMY_CATEGORIES
 
-class CategoryScreen extends StatelessWidget {
-  const CategoryScreen({super.key});
-
+class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: 
-    SingleChildScrollView(
-      child: Row(
-        children: [
-          Text('Category')
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cake Store'),
       ),
-    ));
+      body: GridView(
+        padding: const EdgeInsets.all(15),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(
+                  id: catData.id,
+                  title: catData.title,
+                  imageUrl: catData.imageUrl,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
   }
 }
