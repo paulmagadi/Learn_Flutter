eb3b07dccb800bf427034dbe6b2b1ca9036f1863
SQCodes <paulsqmagadi@gmail.com>
2024-05-29 13:28:15 +0300

    modified:   practice_flutter/cakeAppEcom/myapp/lib/screens/product_details.dart

--- practice_flutter/cakeAppEcom/myapp/lib/screens/product_details.dart
+++ practice_flutter/cakeAppEcom/myapp/lib/screens/product_details.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/cart.dart';
 import '../models/data.dart';
 
-class ProductDetailsScreen extends StatelessWidget {
+class ProductDetailsScreen extends StatefulWidget {
   final String productId;
 
   const ProductDetailsScreen({
@@ -9,8 +11,30 @@ class ProductDetailsScreen extends StatelessWidget {
   });
 
   @override
+  _ProductDetailsScreenState createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  int _quantity = 1;
+
+  void _incrementQuantity() {
+    setState(() {
+      _quantity++;
+    });
+  }
+
+  void _decrementQuantity() {
+    if (_quantity > 1) {
+      setState(() {
+        _quantity--;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final selectedProduct = DUMMY_PRODUCTS.firstWhere((prod) => prod.id == productId);
+    final selectedProduct = DUMMY_PRODUCTS.firstWhere((prod) => prod.id == widget.productId);
+    final cart = Provider.of<Cart>(context, listen: false);
 
     return Scaffold(
       appBar: AppBar(
@@ -37,6 +61,39 @@ class ProductDetailsScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
                 softWrap: true,
               ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.remove),
+                  onPressed: _decrementQuantity,
+                ),
+                Text('$_quantity', style: TextStyle(fontSize: 20)),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: _incrementQuantity,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                cart.addItem(
+                  widget.productId,
+                  selectedProduct.price,
+                  selectedProduct.title,
+                  _quantity,
+                );
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Added to cart!'),
+                    duration: Duration(seconds: 2),
+                  ),
+                );
+              },
+              child: Text('Add to Cart'),
             ),
           ],
         ),
