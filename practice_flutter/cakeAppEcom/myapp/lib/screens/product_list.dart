d0f8a8531651f44b30735fc7388a5735c5153c7c
SQCodes <paulsqmagadi@gmail.com>
2024-05-30 00:38:04 +0300

    modified:   practice_flutter/cakeAppEcom/myapp/lib/screens/product_list.dart

--- practice_flutter/cakeAppEcom/myapp/lib/screens/product_list.dart
+++ practice_flutter/cakeAppEcom/myapp/lib/screens/product_list.dart
@@ -1,40 +1,7 @@
 import 'package:flutter/material.dart';
-// import '../models/data.dart';
+import '../models/data.dart';
 import '../models/product.dart';
 import '../widgets/product_item.dart';
-
-// class ProductsView extends StatelessWidget {
-//   const ProductsView({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return GridView.builder(
-//       // padding: const EdgeInsets.all(15),
-//       itemCount: DUMMY_PRODUCTS.length,
-//       shrinkWrap: true,
-//       physics: const NeverScrollableScrollPhysics(),
-//       gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-//         maxCrossAxisExtent: 200,
-//         childAspectRatio: 2.2/3,
-//         crossAxisSpacing: 5,
-//         mainAxisSpacing: 5,
-//       ),
-//       itemBuilder: (ctx, index) {
-//         final product = DUMMY_PRODUCTS[index];
-//         return ProductItem(
-//           id: product.id,
-//           title: product.title,
-//           imageUrl: product.imageUrl,
-//           price: product.price,
-//         );
-//       },
-//     );
-//   }
-// }
-
-// import 'package:flutter/material.dart';
-// import '../models/data.dart';
-// import '../widgets/product_item.dart';
 
 class ProductsView extends StatelessWidget {
   final List<Product> products;
@@ -44,23 +11,29 @@ class ProductsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
+      // padding: const EdgeInsets.all(15),
+      itemCount: DUMMY_PRODUCTS.length,
       shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      padding: const EdgeInsets.all(10.0),
-      itemCount: products.length,
-      itemBuilder: (ctx, i) => ProductItem(
-        id: products[i].id,
-        title: products[i].title,
-        imageUrl: products[i].imageUrl,
-        price: products[i].price,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 2.2/3,
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
       ),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 2.2 / 3,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-      ),
+      itemBuilder: (ctx, index) {
+        final product = DUMMY_PRODUCTS[index];
+        return ProductItem(
+          id: product.id,
+          title: product.title,
+          imageUrl: product.imageUrl,
+          price: product.price,
+        );
+      },
     );
   }
 }
 
+
+
+// 
