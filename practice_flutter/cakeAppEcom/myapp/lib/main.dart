ae89d61ee1fd158d7f3dbf2d62e6c6999f54e92a
SQCodes <paulsqmagadi@gmail.com>
2024-05-30 11:06:14 +0300

    modified:   practice_flutter/cakeAppEcom/myapp/lib/main.dart

--- practice_flutter/cakeAppEcom/myapp/lib/main.dart
+++ practice_flutter/cakeAppEcom/myapp/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'home_page.dart';
@@ -8,15 +8,15 @@ import 'screens/static/about.dart';
 import 'screens/static/contact.dart';
 import 'screens/static/help.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main()  {
+  // WidgetsFlutterBinding.ensureInitialized();
 
-  if (kIsWeb){
-    await Firebase.initializeApp(options: FirebaseOptions(apiKey: "AIzaSyB8SPpSUlbLRx3EKjpddysoexLlgAebbOk", appId: "1:1028750882492:web:c94de2714fdd84bc5742b9", messagingSenderId: "1028750882492", projectId: "ecommerce-4a53a"));
-  }
+  // if (kIsWeb){
+  //   await Firebase.initializeApp(options: FirebaseOptions(apiKey: "AIzaSyB8SPpSUlbLRx3EKjpddysoexLlgAebbOk", appId: "1:1028750882492:web:c94de2714fdd84bc5742b9", messagingSenderId: "1028750882492", projectId: "ecommerce-4a53a"));
+  // }
 
 
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
   runApp(MyApp());
 }
 
